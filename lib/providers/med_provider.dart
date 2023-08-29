0178ce34fd20387c68c034bee877b2172b9dde4c
Utku Esirgen <utkuesirgen@Utkus-iMac-Pro.local>
2023-08-29 15:53:48 +0300

    initial

---
+++ lib/providers/med_provider.dart
@@ -0,0 +1,13 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+import 'package:ecza/main.dart';
+import 'package:ecza/addingmeds';
+
+class MedProvider with ChangeNotifier {
+  String _isim1 = '';
+
+  String get isim1 => _isim1;
+
+  void GetMedication() {}
+}
